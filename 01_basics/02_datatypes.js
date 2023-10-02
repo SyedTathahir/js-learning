9000a48e3967f2595d06cc51c6704d418ee915cc
Syed Tathahir Uddin <107764144+SyedTathahir@users.noreply.github.com>
2023-10-02 06:28:25 +0000

    added datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict";//treat all js code as newer js version not necessary to write use strict its done automatically now 
+//alert(2+3) we are using nodejs , not browser.
+
+console.log(2+2); 
+console.log("amaan")
+
+
+let name = "hitesh"
+let age = 20
+let islogIn = false
+let state;
+
+
+//number => 2 to power 53
+//bigint  
+// string => ""
+// boolean => true / false
+// null => stand alone value 
+// undefined => 
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof undefined); // undefined 
+console.log(typeof null );// object
+
