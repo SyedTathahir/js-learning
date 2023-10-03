a66016ef7076dd6f620301783957b0f173890df7
Syed Tathahir Uddin <107764144+SyedTathahir@users.noreply.github.com>
2023-10-03 07:59:23 +0000

    added postfix and prefix

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -28,7 +28,7 @@ console.log(stringNumber);
 console.log(typeof stringNumber); */
 
 //********************** OPERATIONS **************************
-
+/* 
 let value = 3 
 let negvalue = -value
 console.log(negvalue);
@@ -41,3 +41,36 @@ console.log(2*2)
 console.log(2**3)
 console.log(2/3)
 console.log(2%5)
+
+let str1 = " hello "
+let str2 = " syed "
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1"+ 2);
+console.log(1 + "2");
+console.log("1"+ 2 + 2);
+console.log(1+ 2 + "2");
+
+console.log(+true);
+console.log(true);
+console.log(+" ");
+//console.log(true+); not to do this 
+*/
+let num1, num2 , num3 
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+
+console.log(gameCounter);
+let levelCounter = ++gameCounter;//f used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
+console.log(gameCounter);
+console.log(levelCounter);
+console.log(gameCounter);
+
+
+
+
+
