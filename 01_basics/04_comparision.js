e4a2951e2ce2afcd0bb6039464ddb9cd14a475d2
Syed Tathahir Uddin <107764144+SyedTathahir@users.noreply.github.com>
2023-10-03 08:11:57 +0000

    strict check done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+// console.log(2>1);
+// console.log(2=1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2!=1);
+
+// console.log("2">1)
+// console.log("02">1)
+
+// console.log(null> 0);
+// console.log(null== 0);
+// console.log(null>= 0);
+// avoid all this conversion not acceptable in industry
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+// console.log(undefined <= 0);
+
+// strict check === checks also data types also 
+
+console.log( "2" === 2);
