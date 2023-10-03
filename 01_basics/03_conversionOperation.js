3ca640327904884278ca091d9938bbb122fb1d62
Syed Tathahir Uddin <107764144+SyedTathahir@users.noreply.github.com>
2023-10-03 08:00:04 +0000

    updated it

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -57,11 +57,11 @@ console.log(+true);
 console.log(true);
 console.log(+" ");
 //console.log(true+); not to do this 
-*/
+
 let num1, num2 , num3 
 
 num1 = num2 = num3 = 2+2
-
+*/
 let gameCounter = 100
 
 console.log(gameCounter);
