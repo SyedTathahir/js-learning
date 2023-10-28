07564714197cb12e37e05a38b1040cfd4d4813ef
Syed Tathahir Uddin <107764144+SyedTathahir@users.noreply.github.com>
2023-10-28 09:31:17 +0000

    explained while and do while loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,25 @@
+// let index = 0 
+// while(index <= 10){
+//     console.log(`value of index is ${index}`);
+//     index = index + 2 ;
+// }
+
+// let myArray = ["flash ","batman ","superman "]
+// let arr = 0 
+// while (arr< myArray.length){
+
+//     console.log(`value is ${myArray[arr]}`);
+//     arr = arr + 1
+// }
+
+let score = 11 // it will execute first then checks the condition
+
+do{
+    console.log(`score is ${score}`);
+    // score = score + 1
+    score++
+}while(score <= 10)
+
+
+
+
