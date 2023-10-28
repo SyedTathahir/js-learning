611ac1e9177780e0bfc5a04323301430a72f4360
Syed Tathahir Uddin <107764144+SyedTathahir@users.noreply.github.com>
2023-10-28 08:48:12 +0000

    truthy / falsy values explained

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -1,8 +1,28 @@
-const userEmail = "a@amaan.ai"
+// const userEmail = "a@amaan.ai"
+const userEmail = []
+// const userEmail2 = ""// empty string
 
-if(userEmail){
-    console.log("Got user email");
-}
-else{
-    console.log("Don't have user email");
+// if(userEmail){
+//     console.log("Got user email");
+// }
+// else{
+//     console.log("Don't have user email");
+// }
+
+//falsy values:
+// false, 0,-0,BigInt 0n ,"", null , undefined,NaN
+// and all other truthy values
+
+// truthy values:
+// "0",'false'," ",[],{},function(){}empty function,
+
+
+// if( userEmail.length===0){
+//     console.log("Array is Empty");
+// }
+
+const emptyObject= {}
+
+if(Object.keys(emptyObject).length === 0){
+    console.log("object is empty");
 }
