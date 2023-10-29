7ef75b2de91e0a2182a4640b91ec295bb3dd5a57
Syed Tathahir Uddin <107764144+SyedTathahir@users.noreply.github.com>
2023-10-29 18:23:02 +0000

    tested maps with for in loop

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -7,6 +7,27 @@ const myObject = {
     swift: 'Swift by Apple'
 }
 
-for (const key in  myObject){
-    console.log(`${key} shortcut is for ${myObject[key]}`);
+// for (const key in  myObject){
+//     console.log(`${key} shortcut is for ${myObject[key]}`);
+// }
+// for in loop for Arrays:
+
+const programming = ["js","rb","py","java","cpp"]
+
+for (const key  in programming) {
+    console.log(programming[key]);   
 }
+
+// for in loop for Maps:
+
+// const map = new Map()
+// map.set('IN',"India")
+// map.set('USA',"United States of America")
+// map.set('FR',"France")
+// map.set('IN',"India")
+
+// for (const key in map) {// Maps are not iterable  with for in loop.
+//     console.log(key);
+// }
+
+
