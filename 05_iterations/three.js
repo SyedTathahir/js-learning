c9b5acce412978b0f2d9284cdb6057227f5506fd
Syed Tathahir Uddin <107764144+SyedTathahir@users.noreply.github.com>
2023-10-29 18:04:10 +0000

    intro of maps

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -4,7 +4,21 @@
 // [{},{},{}]
 
 
-const arr = [1,2,3,4,5]
-for (const iterator of arr) {
-    console.log(iterator)
+// const arr = [1,2,3,4,5]
+// for (const iterator of arr) {
+//     console.log(`Number is : ${iterator}`)
+// }
+
+const greeting  = "Hello Syed"
+for (const greet of greeting){
+    console.log(`Each character is : ${greet}`);
+    
 }
+
+//Maps:
+
+const map = new Map()
+map.set('IN',"India ")
+map.set('USA',"United States of America")
+
+
