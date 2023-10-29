dc4a3bbd208a38f97be66ca3242f214deba439a9
Syed Tathahir Uddin <107764144+SyedTathahir@users.noreply.github.com>
2023-10-29 18:12:58 +0000

    explained maps wtih for of loop

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -9,16 +9,39 @@
 //     console.log(`Number is : ${iterator}`)
 // }
 
-const greeting  = "Hello Syed"
-for (const greet of greeting){
-    console.log(`Each character is : ${greet}`);
+// const greeting  = "Hello Syed"
+// for (const greet of greeting){
+//     console.log(`Each character is : ${greet}`);
     
-}
+// }
 
 //Maps:
 
 const map = new Map()
-map.set('IN',"India ")
+map.set('IN',"India")
 map.set('USA',"United States of America")
+map.set('FR',"France")
+map.set('IN',"India")// it does not store duplicate value,it stores unique value and it remembers the order of key value pairs.
+
+console.log(map);
+
+// for of loop on maps:
+
+for (const [key,value]  of map) {
+    console.log(key,':-',value);
+}
+
+// for of loop on object:
+
+const myObject = {
+    'game1': 'NFS',
+    game2: 'Spiderman'
+}
+
+for (const [key,value] of myObject) {// objects are not iterable with for of loop.
+    console.log(key,':-',value);
+    
+}
+
 
 
