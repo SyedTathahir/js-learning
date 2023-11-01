12542751dbce1addd747fd10e2982045dfa03dc8
Syed Tathahir Uddin <107764144+SyedTathahir@users.noreply.github.com>
2023-11-01 06:32:53 +0000

    added one more example

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -14,7 +14,25 @@ const coding = ["js","ruby","java","python","cpp"]
 
 // coding.forEach(printMe)
 
-coding.forEach( (item,index,arr)=>{
-    console.log(item,index,arr)
-})
+// coding.forEach( (item,index,arr)=>{
+//     console.log(item,index,arr)
+// })
 
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "Java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+myCoding.forEach( (item)=> {
+    console.log(item.languageName)
+})
