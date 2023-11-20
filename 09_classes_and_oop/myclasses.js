8be5a2479445cfa4e7569fec6ac37a8af282c9b8
Syed Tathahir Uddin <107764144+SyedTathahir@users.noreply.github.com>
2023-11-20 06:36:50 +0000

    behind the scenes of class with prototype

---
+++ 09_classes_and_oop/myclasses.js
@@ -0,0 +1,39 @@
+// class User{
+//     constructor(username,email,password){
+//         this.username = username;
+//         this.email= email;
+//         this.password = password
+//     }
+//     encryptPassword(){
+//         return  `${this.password} abc`
+//     }
+//     changeUsername(){
+//         return`${this.username.toUpperCase()}`
+//     }
+// }
+
+// const chai = new User("chai","chai@example.com","123")
+
+// console.log(chai.encryptPassword());
+// console.log(chai.changeUsername());
+
+// behind the scene:
+
+function User(username,email,password){
+    this.username = username;
+    this.email= email;
+    this.password = password
+}
+
+User.prototype.encryptPassword = function(){
+    return `${this.password}abc`
+}
+User.prototype.changeUsername = function(){
+    return `${this.username.toUpperCase()}`
+}
+
+const tea = new User("tea","tea@example.com","456");
+
+console.log(tea.encryptPassword());
+console.log(tea.changeUsername());
+
