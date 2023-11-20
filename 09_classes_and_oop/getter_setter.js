8d0ce28118b5a8708e0e2012a4fc6962590e71f8
Syed Tathahir Uddin <107764144+SyedTathahir@users.noreply.github.com>
2023-11-20 08:35:14 +0000

    getters and setters done

--- 09_classes_and_oop/getter_setter.js
+++ 09_classes_and_oop/getter_setter.js
@@ -3,13 +3,20 @@ class User{
         this.email = email
         this.password = password
     }
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(value){
+         this._email = value// setters don't return value 
+    }
     get password(){
         return  `${this._password}syed`
     }
     set password(value){
-        this._password = value
+        this._password = value//change name like _password
     }
 }
 
 const syed = new User ("syed@example.com","abc")
 console.log(syed.password);
+console.log(syed.email);
