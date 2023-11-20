b3246e3fe9fa2e2bb84b83958842a341b47dc48a
Syed Tathahir Uddin <107764144+SyedTathahir@users.noreply.github.com>
2023-11-20 07:03:59 +0000

    static keyword done

--- 09_classes_and_oop/staticprop.js
+++ 09_classes_and_oop/staticprop.js
@@ -1,5 +1,28 @@
 class User {
     constructor(username){
-        
+        this.username = username;
+
+    }
+    logMe(){
+        console.log(`Username: ${this.username}`);
+    }
+    static createId(){
+        return `123`
     }
 }
+
+const syed = new User("syed");
+// console.log(syed.createId());static keyword restricts to access objects outside the class
+
+class Teacher extends User{
+    constructor(username,email){
+        super(username)
+        this.email = email;
+    }
+}
+
+const iphone = new Teacher("iphone","i@phone.com");
+
+iphone.logMe();
+console.log(iphone.createId())
+
