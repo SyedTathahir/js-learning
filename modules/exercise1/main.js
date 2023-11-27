7e994537edc6f9cfba49d2cca034367316da247e
Syed Tathahir Uddin <107764144+SyedTathahir@users.noreply.github.com>
2023-11-27 09:05:11 +0000

    added files

---
+++ modules/exercise1/main.js
@@ -0,0 +1,6 @@
+import greeting,{square} from './util.js'// only one default export is allowed
+
+const num = square(6)
+console.log(num);
+
+greeting("monkey")
---
+++ modules/exercise1/util.js
@@ -0,0 +1,8 @@
+const square = (number)=>{
+    return number*number;
+}
+const greeting = (name)=>{
+    return console.log(`Hello ${name}`);
+}
+export {square};
+export default greeting;
