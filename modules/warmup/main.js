64bfbba2caaf2fcef3421f2e12881a4f50322fd5
Syed Tathahir Uddin <107764144+SyedTathahir@users.noreply.github.com>
2023-11-27 05:48:06 +0000

    added files for modules

---
+++ modules/warmup/main.js
@@ -0,0 +1,5 @@
+import {capsLock}from'./util.js'
+
+const caps = capsLock("hello syed")
+
+console.log(caps);
---
+++ modules/warmup/util.js
@@ -0,0 +1,5 @@
+const capsLock = (string)=>{
+    return string.toUpperCase()
+}
+
+export {capsLock};
