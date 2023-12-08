195289c57ad98d8b654eb88a601f225ab7921384
Syed Tathahir Uddin <107764144+SyedTathahir@users.noreply.github.com>
2023-12-08 10:20:20 +0000

    parseInt and parseFloat done

---
+++ cohort2.0/week1/integers.js
@@ -0,0 +1,17 @@
+// parseInt converts strings into numbers
+
+console.log("ParseInt");
+console.log(parseInt("42"))
+console.log(parseInt("42px"))
+console.log(parseInt("42pfdjljdsfjx"))
+console.log(parseInt("afaj42px"))
+console.log(parseInt("3.14"))
+
+// parsefloat converts strings into float
+console.log("ParseFloat");
+console.log(parseFloat("42"))
+console.log(parseFloat("42px"))
+console.log(parseFloat("42pfdjljdsfjx"))
+console.log(parseFloat("afaj42px"))
+console.log(parseFloat("3.14"))
+
