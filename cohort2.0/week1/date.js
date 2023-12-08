b31f4e718ee00b006c3fbe42c2e4e4ce39e4df5d
Syed Tathahir Uddin <107764144+SyedTathahir@users.noreply.github.com>
2023-12-08 14:21:02 +0000

    counter done

--- cohort2.0/week1/date.js
+++ cohort2.0/week1/date.js
@@ -19,19 +19,25 @@ console.log("After setFullYear: "+ currentDate);
 
 // setTime:
 
-console.log("Time in milliseconds since 1970: ", currentDate.getTime()); // epoch timestamp 
+// console.log("Time in milliseconds since 1970: ", currentDate.getTime()); // epoch timestamp 
 
-function calculateSum (){
-    let a = 0
-    for (let  i = 0 ;i<100000000 ;i++){
-        a  = a+i 
-    }
-    return a ;
+// function calculateSum (){
+//     let a = 0
+//     for (let  i = 0 ;i<100000000 ;i++){
+//         a  = a+i 
+//     }
+//     return a ;
+// }
+// const beforeDate = new Date()
+// const beforeTimeInMs =beforeDate.getTime();
+// calculateSum();
+// const afterDate = new Date();
+// const afterTimeInMs = afterDate.getTime();
+
+// console.log(afterTimeInMs - beforeTimeInMs)
+
+// counter: 
+function currentTimePrint(){
+    console.log(new Date().getTime());
 }
-const beforeDate = new Date()
-const beforeTimeInMs =beforeDate.getTime();
-calculateSum();
-const afterDate = new Date();
-const afterTimeInMs = afterDate.getTime();
-
-console.log(afterTimeInMs - beforeTimeInMs)
+setInterval(currentTimePrint, 1000)
