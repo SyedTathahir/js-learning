eb71af3213379ddd9930069311fce03657780559
Syed Tathahir Uddin <107764144+SyedTathahir@users.noreply.github.com>
2023-12-09 15:05:46 +0000

    promises done

--- cohort2.0/week2/promises.js
+++ cohort2.0/week2/promises.js
@@ -1,1 +1,24 @@
-console.log("hi there")
+// callback eg:
+
+function mysettimeOut(callback,duration){
+    setTimeout(callback,duration);
+}
+mysettimeOut(function(){
+    console.log("hello callback")
+},1000)
+// promisify function:
+
+function promisifymysettimeOut(duration ){
+    let p = new Promise(function (resolve){
+        setTimeout(()=>{resolve()
+        },duration)
+    })
+    return p;
+
+
+}
+const ans = promisifymysettimeOut(3000)
+console.log(ans);
+ans.then(function(){
+    console.log("hello promise")
+})
